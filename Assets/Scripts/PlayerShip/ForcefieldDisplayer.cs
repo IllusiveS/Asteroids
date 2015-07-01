8625779834af8b12f7421b0db0d5ed13f8092b44
IllusiveS <wysockipawelpatryk@gmail.com>
2015-07-01 08:18:12 +0200

    displaying invincibility

---
+++ Assets/Scripts/PlayerShip/ForcefieldDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+
+public class ForcefieldDisplayer : MonoBehaviour {
+    protected SpriteRenderer sprite;
+
+    [Inject]
+    protected IPlayer controller;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sprite.enabled = controller.isInvincible;
+	}
+}
